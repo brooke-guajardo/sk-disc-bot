32737a33fe8f528470d37d467dc96bea35340409
Brooke Guajardo <brooke.guajardo@wnco.com>
2022-04-08 15:50:51 -0500

    button test

--- cogs/game.py
+++ cogs/game.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ui import Button
 import asyncio
 import datetime
 import sqlite3
@@ -210,6 +211,11 @@ class GameCog(commands.Cog, name='Game'):
         embed.add_field(name="**Presence**", value=f"{presence}", inline=True)
         embed.set_thumbnail(url=f"{ctx.author.avatar_url}")
         await ctx.send(embed=embed)
+
+        button = Button(label="Ping", style=discord.ButtonStyle.green,)
+        view = View()
+        view.add_item(button)
+        await ctx.send("Pong!", view=view)
 
 # list out player attributes
     @commands.command()
