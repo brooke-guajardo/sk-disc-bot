25e06de0a6993c04bf59e50074237985e9fca878
Brooke Guajardo <brooke.guajardo@wnco.com>
2022-04-09 01:43:56 -0500

    hrmm

--- cogs/test.py
+++ cogs/test.py
@@ -8,13 +8,13 @@ import random
 
 class TestCog(commands.Cog, name='Test'):
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
     @commands.command()
     async def pings(self, ctx):
         await ctx.send(f"Meow meow meow")
 
-async def setup(bot):
+async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(TestCog(bot))
     print('Test Cog is loaded.')
