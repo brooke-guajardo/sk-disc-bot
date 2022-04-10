598b89f11ea0b77cdbefda6d3069e010c07b6344
Brooke Guajardo <brooke.guajardo@wnco.com>
2022-04-10 16:24:53 -0500

    message content is empty, trying all intent access

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-intents = discord.Intents(messages=True)
-intents.typing = False
-intents.presences = False
+intents = discord.Intents.all()
+#intents.typing = False
+#intents.presences = False
 initial_extensions = ['cogs.sql', 'cogs.cards', 'cogs.game', 'cogs.dm', 'cogs.test']
 
 class SKBot(commands.Bot):
