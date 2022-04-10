064fd7adf74f5bab8dc0e5b8c3e6293add79d346
Brooke Guajardo <brooke.guajardo@wnco.com>
2022-04-10 15:37:57 -0500

    logging

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ import random
 import sqlite3
 import json
 import aiohttp
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 intents = discord.Intents(messages=True)
 intents.typing = False
